d132c1ae2c10f6e11137d097c649dda9c696862e
Jaumz <joaopedro.viana07@gmail.com>
2023-04-01 08:59:10 -0300

    feat: Add info

--- src/components/AddData/AddData.js
+++ src/components/AddData/AddData.js
@@ -1,31 +1,36 @@
 import './AddData.css';
-import { useState} from 'react';
+import { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { useNavigate } from 'react-router-dom';
 import ApiCall from '../../API';
 import Header from '../Header/Header';
 
-const AddData = ({ setAddData, setRefresh, refresh }) => {
+const AddData = () => {
 
     const { register } = useForm();
     const navigate = useNavigate();
-
-    const [name, setName] = useState();
-    const [address, setAddress] = useState();
-    const [city, setCity] = useState();
-    const [uf, setUf] = useState();
-    const [cell, setCell] = useState();
+    const [name, setName] = useState("");
+    const [address, setAddress] = useState("");
+    const [city, setCity] = useState("");
+    const [UF, setUF] = useState("");
+    const [phone, setPhone] = useState("");
 
     const createClient = async (e) => {
         e.preventDefault();
+        const client = { 
+            TECL_NOME: name, 
+            TECL_ENDERECO: address, 
+            TECL_CIDADE: city, 
+            TECL_UF: UF, TECL_TELEFONE: phone };
 
-        const client = { name, address, city, uf, cell };
+        try {
+            await ApiCall.post("/cliente", client)
+        } catch (error) {
+            console.log(error)
+        }
 
-        await ApiCall.post("/cliente", client, {})
-
-        navigate('/home');
+         navigate('/home');
     };
-
 
     // const BlankInfo = () => {
     // if (name !== "" || address !== "" || city !== "" || state !== "" || cell !== "") {
@@ -40,17 +45,22 @@ const AddData = ({ setAddData, setRefresh, refresh }) => {
                     <form className="form_container" onSubmit={(e) => createClient(e)}>
                         <div className="first-line">
                             <label htmlFor="input1">Nome:</label>
-                            <input type="text" id="input1" {...register("TECL_NOME", { onChange: (e) => setName(e.target.value) })} />
+                            <input type="text" id="input1" value={name}
+                                onChange={(e) => setName(e.target.value)} />
                             <label htmlFor="input2">Endereço:</label>
-                            <input type="text" id="input2" {...register("TECL_ENDERECO", { onChange: (e) => setAddress(e.target.value) })} />
+                            <input type="text" id="input2" value={address}
+                                onChange={(e) => setAddress(e.target.value)} />
                         </div>
                         <div className="second-line">
                             <label htmlFor="input3">Cidade:</label>
-                            <input type="text" id="input3" {...register("TECL_CIDADE", { onChange: (e) => setCity(e.target.value) })} />
+                            <input type="text" id="input3" value={city}
+                                onChange={(e) => setCity(e.target.value)} />
                             <label htmlFor="input4">UF:</label>
-                            <input type="text" id="input4" {...register("TECL_UF", { onChange: (e) => setUf(e.target.value) })} />
+                            <input type="text" id="input4" value={UF}
+                                onChange={(e) => setUF(e.target.value)} />
                             <label htmlFor="input5">Telefone:</label>
-                            <input type="text" id="input5" {...register("TECL_TELEFONE", { onChange: (e) => setCell(e.target.value) })} />
+                            <input type="text" id="input5" value={phone}
+                                onChange={(e) => setPhone(e.target.value)} />
                         </div>
                         <button type="submit" onClick={(e) => createClient(e)}>Salvar</button>
                     </form>
